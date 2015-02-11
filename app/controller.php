2960fb5539e9aacce46e16478b20bb9f2e99b410
orlandoGAero <orlando_30@outlook.com>
2015-02-11 13:55:26 -0600

    agregue archivos de controller y model funcion proveedores

--- app/controller.php
+++ app/controller.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Esta clase implementa una serie de métodos públicos, que hemos denominado acciones para indicar que son métodos asociados a URL’s. 
- * En cada una de las acciones se declara un array asociativo con los datos que serán pintados en la plantilla. Pero en ningún caso hay información 
- * acerca de como se pintarán dichos datos. Por otro lado, casi todas las acciones utilizan un objeto de la clase Models para realizar operaciones relativas
- * a la lógica de negocio.
- */
     class controller{
     	public function inicio(){
     		$obtenerDatos = array(
@@ -13,106 +7,6 @@
 			
 			require __DIR__ . '/templates/inicio.php';
     	}
-		
-		//CONTACTOS
-		public function listarContacto()
-		{
-			$m = new model(config::$mvc_db_name, config::$mvc_db_user,
-						config::$mvc_db_pass, config::$mvc_db_hostname);
-						
-			$obtenerDatosContactos = array(
-				'contactos' => $m->obtenerContactos(),
-			); 
-			
-			require __DIR__ . '/templates/mostrarContactos.php';
-		}
-		
-		public function verContacto(){
-			if(!isset($_GET['idContact'])){
-				throw new Exception("Página no encontrada", 1);
-			}
-			
-			$IdContacto = $_GET['idContact'];
-			
-			$m = new model(config::$mvc_db_name, config::$mvc_db_user,
-						config::$mvc_db_pass, config::$mvc_db_hostname);
-			
-			$detalleContacto = $m->obtenerContacto($IdContacto);
-			
-			$obtenerDatosContacto = $detalleContacto;
-			
-			require __DIR__ . '/templates/verContacto.php';
-			
-		}
-		
-		public function insertarContacto(){
-				
-			$m = new model(config::$mvc_db_name, config::$mvc_db_user,
-						config::$mvc_db_pass, config::$mvc_db_hostname);
-										
-			$parametrosContactos = array(
-				//Datos Contacto
-				'idCont' => $m->obtenerIdContacto(),
-				'nombre' => '',
-				'app' => '',
-				'apm' => '',
-				'area' => '',
-				'movil' => '',
-				'tel_ofi' => '',
-				'tel_emer' => '',
-				'correoPers' => '',
-				'correoInsti' => '',
-				'RSFacebook' => '',
-				'RSTwitter' => '',
-				'RSSkype' => '',
-				'pagWeb' => '',
-				//Datos dirección física
-				'idDir' => $m->incrementoDir(),
-				'calleD' => '',
-				'numExterior' => '',
-				'numInterior' => '',
-				'coloniaD' => '',
-				'referenciaD' => '',
-			);
-			
-			
-			
-			if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-			
-				if($m->registrarContacto($_POST['idAddress'],$_POST['street'],$_POST['numExt'],$_POST['numInt'],$_POST['colonia'],$_POST['reference'],
-						$_POST['idContact'],$_POST['nameContact'],$_POST['ApPContact'],$_POST['ApMContact'],$_POST['nameArea'],$_POST['telMovil'],$_POST['telOficina'],
-						$_POST['telEmergencia'],$_POST['emailPersonal'],$_POST['emailInstitucional'],$_POST['redSocialF'],$_POST['redSocialT'],$_POST['redSocialS'],
-						$_POST['webPage'])){
-							header('Location: index.php?url=listContact');
-				} else {
-						$parametrosContactos = array(
-						'idCont' => $_POST['idContact'],
-						'nombre' => $_POST['nameContact'],
-						'app' => $_POST['ApPContact'],
-						'apm' => $_POST['ApMContact'],
-						'area' => $_POST['nameArea'],
-						'movil' => $_POST['telMovil'],
-						'tel_ofi' => $_POST['telOficina'],
-						'tel_emer' => $_POST['telEmergencia'],
-						'correoPers' => $_POST['emailPersonal'],
-						'correoInsti' => $_POST['emailInstitucional'],
-						'RSFacebook' => $_POST['redSocialF'],
-						'RSTwitter' => $_POST['redSocialT'],
-						'RSSkype' => $_POST['redSocialS'],
-						'pagWeb' => $_POST['webPage'],
-						'idDir' => $_POST['idAddress'],
-						'calleD' => $_POST['street'],
-						'numExterior' => $_POST['numExt'],
-						'numInterior' => $_POST['numInt'],
-						'coloniaD' => $_POST['colonia'],
-						'referenciaD' => $_POST['reference'],
-					);
-					$parametrosContactos['mensaje'] = 'Error al registrar contactos. Revise el formulario';
-				}
-			}
-			
-			require __DIR__.'/templates/insertarContacto.php';
-		}
 		
 		/* CODIGOS POSTALES */
 		public function listarCodPost()
@@ -126,27 +20,7 @@
 			
 			require __DIR__ . '/templates/mostrarCodigosPostales.php';
 		}
-			
-		public function verCodPost()
-		{				
-			if (!isset($_GET['idCP'])) {
-				throw new Exception("Página no encontrada", 1);
-			}
-			
-			
-			$idCodPost = $_GET['idCP'];
-			//$idCodPost = Encrypter::decrypt("$idCodigoPostal");
-			
-			$m = new model(config::$mvc_db_name, config::$mvc_db_user,
-						config::$mvc_db_pass, config::$mvc_db_hostname);
-						
-			$codPost = $m->obtenerCodigoPostal($idCodPost);
-			
-			$obtenerDatos = $codPost;
-			
-			require __DIR__ . '/templates/verCodigoPostal.php';
-		}
-    
+		
 		public function insertarCodPost()
 		{
 			
@@ -158,83 +32,43 @@
 			
 			require __DIR__ . '/templates/________';
 		}
-
-
-		/*---------------------------------------------CLIENTES-------------------------------------------*/
-		public function listaCliente()
-		{
-			/*variable de conexion*/	
-			$m = new model(config::$mvc_db_name, config::$mvc_db_user,
-						config::$mvc_db_pass, config::$mvc_db_hostname);
-						
-			$obtenerCliente = array(
-				'm_clientes' => $m->obtieneClientes(),
-			);
-			require __DIR__ . '/templates/clientes/mostrarCliente.php';
-    	}
-    	
-    	public function verCliente()
-    	{
-    		if(!isset($_GET['id_cli'])){
-    			throw new Exception("Página no encontrada", 1);    			
-    		}
-    		$cv_cli=$_GET['id_cli'];
-
-			$m = new model(config::$mvc_db_name, config::$mvc_db_user,
-						config::$mvc_db_pass, config::$mvc_db_hostname);
-    	
-			$lisCliente=$m->obtieneVcliente($cv_cli);		
-			$obtenerCliente=$lisCliente;					
-			require __DIR__ . '/templates/clientes/verCliente.php';
-    	}
-    		//funcion para el formulario de nuevo_cliente
-    		public function agregarCl()
-    		{
-    			
-			$m = new model(config::$mvc_db_name, config::$mvc_db_user,
-						config::$mvc_db_pass, config::$mvc_db_hostname);
-
-			require __DIR__ . '/templates/clientes/nuevoCl.php';
-    	}
-    	
-    	public function eli_cli()
-    	{
-    		if(!isset($_GET['id_cli'])){
-    			throw new Exception("Página no encontrada", 1);    			
-    		}
-    		$del_cli=$_GET['id_cli'];
-			$m = new model(config::$mvc_db_name, config::$mvc_db_user,
-						config::$mvc_db_pass, config::$mvc_db_hostname);
-    	
-			$lisCliente=$m->elimCliente($del_cli);		
-			$obtenerCliente=$lisCliente;				
-			require __DIR__ . '/templates/clientes/elim.php';
-    	}
 		
-		//funcion para el formulario de editar_cliente
-    	public function modCl()
-    		{	
-    	
-			require __DIR__ . '/templates/clientes/modificar_cl.php';
-    	}
-
-    	
-    	public function buscarXC()
-    	{
-    	$crit = array(
-			'busqueda' => '',
-			'resultado' => array(),
-			);
+		public function verCodPost()
+		{				
+			if (!isset($_GET['idCP'])) {
+				throw new Exception("Página no encontrada", 1);
+			}
+			
+			$idCodPost = $_GET['idCP'];
 			
 			$m = new model(config::$mvc_db_name, config::$mvc_db_user,
 						config::$mvc_db_pass, config::$mvc_db_hostname);
 						
-			if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-             $crit['busqueda'] = $_POST['busqueda'];
-             $crit['resultado'] = $m->busquedaX($_POST['busqueda']);
-         }
-		 
-			require __DIR__ . '/templates/clientes/buscador.php';
-    	}
-}
+			$codPost = $m->obtenerCodigoPostal($idCodPost);
+			
+			$obtenerDatos = $codPost;
+			
+			require __DIR__ . '/templates/verCodigoPostal.php';
+		}
+
+		// FUNCIONES PROVEEDORES
+
+		public function Proveedor()
+		{
+			$model = new model(config::$mvc_db_name, config::$mvc_db_user,
+						config::$mvc_db_pass, config::$mvc_db_hostname);
+
+			$obtenerDat = array('proveedores' => $model->obtenerProveedores(),);
+
+			require '/templates/proveedor/mostrarProveedores.php';
+		}
+
+		public function InsertarProveedor()
+		{
+			$model = new model(config::$mvc_db_name, config::$mvc_db_user,
+						config::$mvc_db_pass, config::$mvc_db_hostname);
+
+			require '/templates/proveedor/nuevoPro.php';
+		}
+    }
 ?>
